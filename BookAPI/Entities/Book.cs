aecd73e1b28f3bc3c79c90c5f60df3acb1330a59
zabiulla <zabiulla.edu@gamil.com>
2022-06-12 00:47:11 +0530

    added Data Annotation attribute like Range, Required

--- BookAPI/Entities/Book.cs
+++ BookAPI/Entities/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,21 @@ namespace BookAPI.Entities
 {
     public record Book
     {
+        [Required]
+        [Range(0, 100)]
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
         public string Author { get; set; }
+
+        [Range(1,1000)]
         public int Prize { get; set; }
+
         public DateTimeOffset Date { get; set; }
+
         public string Discription { get; set; }
+
     }
 }
